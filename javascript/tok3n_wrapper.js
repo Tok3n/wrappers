f81e3b77725154330158c2a5f1140811bdfe064b
Alexandro Colorado <jza@oooes.org>
2014-08-20 17:34:30 -0500

    added directories for the target languages

---
+++ javascript/tok3n_wrapper.js
@@ -0,0 +1,21 @@
+// tok3n_wrapper.js
+
+////////////////////////////////////////////
+//
+//        Tok3n wrapper client for APIv2
+//        By Tok3n Solutions SAPI CV
+//        September 2014
+//
+//        Basic use:
+// js> load('tok3n_wrapper.js');
+// js> tok3n.client('client_id', 'api_key');
+//
+////////////////////////////////////////////
+
+var client = (function() {
+  
+})();
+
+var validation = (function() {
+  
+})();
