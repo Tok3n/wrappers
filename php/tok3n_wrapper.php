a3762ef43e3bb3062691d2abd45dd675011be233
Alexandro Colorado <jza@oooes.org>
2014-08-20 17:34:46 -0500

    added directories for the target languages

---
+++ php/tok3n_wrapper.php
@@ -0,0 +1,26 @@
+<?php
+
+/* ***********************************************
+*
+*            Tok3n wrapper client for APIv2
+*            By Tok3n Solutions SAPI CV
+*            September 2014
+*
+*           Basic use:
+*  include once tok3n.php
+*  echo tok3n::client('client_id', 'api_key')
+*
+*************************************************/
+
+class client {
+
+   public function init(self, client_id, api_key, host=API_HOST, port=API_PORT) {
+      
+   }
+
+}
+
+class validation {
+
+
+}
