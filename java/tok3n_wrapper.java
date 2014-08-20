313d38b7379ca85d9796cda0a9f3b2ad70d679eb
Alexandro Colorado <jza@oooes.org>
2014-08-20 17:34:19 -0500

    added directories for the target languages

---
+++ java/tok3n_wrapper.java
@@ -0,0 +1,27 @@
+/************************************************
+*
+*           Tok3n wrapper client for APIv2
+*           By Tok3n solucions SAPI CV
+*           September 2014
+* 
+*   Basic usage:
+* 
+* 
+* 
+* 
+*************************************************/
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class client {
+
+
+
+}
+
+public class validation {
+
+}
