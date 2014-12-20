6c1e58fd5db8569d420d20c38a087a0ba254ba6c
Alexandro Colorado <jza@oooes.org>
2014-12-20 14:25:22 -0600

    Update tok3n_wrapper.java
    Update client from server

--- java/tok3n_wrapper.java
+++ java/tok3n_wrapper.java
@@ -11,17 +11,58 @@
 * 
 *************************************************/
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.BufferedReader;
 
-public class client {
+/*   Tok3n REST Client
+ * Wrapper generate a service URL 
+ * send parameters to the Tok3n.js API
+ *
+ * @ Alexandro Colorado
+ */
 
+public class Tok3nClient { 
 
+    public static void ValidateAuth(String[] args) throws ProtocolException, IOException {
+	try {
 
+	     URL url= new URL("http://secure.tok3n.com/api_v2_iframe/tok3n.js");
+	     HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+	     conn.setRequestMethod("POST");
+	     conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+	     conn.setRequestProperty("Content-Laguage", "en-US");
+	     conn.setDoOutput(true);
+	     conn.setUseCaches(false);
+	     conn.setDoInput(true);
+	     DataOutputStream wr = new DataOutputStream (
+		     con.getOutputStream ());
+	     wr.writeBytes (urlParameters);
+	     wr.flush ();
+	     wr.close ();
+
+	if ( conn.getResponseCode() != 200) {
+	    throw new RuntimeException("Failed: HTTP error code : " +
+		    conn.getResponseCode());
+	}
+
+	BufferedReader br = new BufferedReader( new InputStreamReader(
+		    (conn.getInputStream())));
+
+	String output;
+	System.out.println("Output from Server .... \n");
+	while ((output = br.readLine()) != null) {
+	    System.out.println(output);
+	}
+	conn.disconnect();
+    } catch (MalformedURLException e) {
+	e.printStackTrace();
+    } catch (IOException e) {
+	e.printStackTrace();
+    }
 }
-
-public class validation {
-
 }
